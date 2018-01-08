843a237033c854e74bdcb6e8ccb73b4351e84800
Ashvala Vinay <mail@ashvala.net>
2018-01-08 16:33:58 +0530

    refactor the stackchain Unit system.

---
+++ PaperSynth/stackChain/Equalizer.swift
@@ -0,0 +1,9 @@
+//
+//  equalizer.swift
+//  PaperSynth
+//
+//  Created by Ashvala Vinay on 1/8/18.
+//  Copyright © 2018 Ashvala Vinay. All rights reserved.
+//
+
+import Foundation
--- PaperSynth/stackChain/StackChainUnit.swift
+++ PaperSynth/stackChain/StackChainUnit.swift
@@ -11,16 +11,32 @@ import Foundation
 
 protocol SCUnit {
     func getUI() -> [PSRotaryKnob]
+    init()
+    func getNode() -> AKNode
 }
 
-enum Types: String {
-    case oscil = "SCOscil"
-    case mic = "SCMic"
-    case delay = "SCDelay"
-    case mixer = "SCMixer"
-    
-    var getClass: AnyClass{
-        return NSClassFromString(self.rawValue)!
+extension SCUnit {
+    func entity() -> Self {
+        return Self()
+    }
+}
+
+enum Types {
+    case oscil
+    case mic
+    case delay
+    case mixer
+    func getObject() -> SCUnit {
+        switch self {
+        case .oscil:
+            return SCOscil()
+        case .mic:
+            return SCMic()
+        case .delay:
+            return SCDelay()
+        case .mixer:
+            return SCMixer()
+        }
     }
 }
 
@@ -28,12 +44,14 @@ struct stackChainUnit {
 
     var name: String
     var type: Types
-    var unitClass: AnyClass!
+    var unitClass: SCUnit!
+    var canInput: Bool
 
-    init(name: String, type: Types) {
+    init(name: String, type: Types, canInput: Bool) {
         self.name = name
         self.type = type
-        self.unitClass = self.type.getClass
+        self.unitClass = self.type.getObject()
+        self.canInput = canInput
     }
 
 }
